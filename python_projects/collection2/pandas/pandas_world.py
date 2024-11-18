a4acfe3a87cddc9a3c7867b829839e332c96b519
hsnya <153435888+hsnya@users.noreply.github.com>
2024-11-19 01:47:58 +0300

    Learning Pandas.

--- python_projects/collection2/pandas/pandas_world.py
+++ python_projects/collection2/pandas/pandas_world.py
@@ -53,6 +53,96 @@ def querying():
     print([pd.__version__])
     
 
+def introDataFrames():
+    students = [{'Name': 'Alice',
+              'Class': 'Physics',
+              'Score': 85},
+            {'Name': 'Jack',
+             'Class': 'Chemistry',
+             'Score': 82},
+            {'Name': 'Helen',
+             'Class': 'Biology',
+             'Score': 90}]
+    
+    df = pd.DataFrame((students),
+                      index=['school1', 'school2', 'school1'])
+    
+    print(df.head())
+    print(df.loc['school1', 'Name'])
+    print(df['Name'])
+    
+    print(df.T)
+    print(df.T.loc['Name'])
+    
+    print(df.drop('Name', axis=1, inplace=True))
+    del df['Class']
+    print(df)
+    
+    df['Class'] = None
+    print(df)
+
+
+def dataFramesIndexLoad():
+    df = pd.read_csv(r'python_projects\collection2\pandas\inputs\Admission_Predict.csv', index_col=0)
+    df = df.rename(mapper=str.strip, axis='columns')
+    df = df.rename(columns={'SOP': 'Statement of Purpose',
+                            'LOR': 'Letter of Recommendation'})
+    print(df.head())
+    
+    df.columns = [x.upper() for x in list(df.columns)]
+    print(df.head())
+
+
+def dataFramesQuerying():
+    df = pd.read_csv(r'python_projects\collection2\pandas\inputs\Admission_Predict.csv', index_col=0)
+    df = df.rename(mapper=lambda x: x.strip().lower(), axis='columns')
+    
+    print(df.head())
+    
+    mask = (df['chance of admit'] > 0.8) | (df['gre score'] < 320) # |
+    mask = df['chance of admit'].gt(0.8, ) & df['gre score'].lt(320)
+    print(df.where(mask).dropna().head())
+
+
+def dataFramesIndexing():
+    df = pd.read_csv(r'python_projects\collection2\pandas\inputs\Admission_Predict.csv', index_col=0)
+    df = df.rename(mapper=lambda x: x.strip().lower(), axis='columns')
+    
+    print(df.head())
+    
+    df['serial number'] = df.index
+    df = df.set_index('chance of admit')
+    print(df.head())
+    
+    df = df.reset_index()
+    print(df.head())
+    
+    df = pd.read_csv(r'python_projects\collection2\pandas\inputs\census.csv')
+    print(df.head())
+    
+    print(df['SUMLEV'].unique())
+    
+    df = df.set_index(['STNAME', 'CTYNAME'])
+    print(df.head())
+
+    print(df.loc[ [('Michigan', 'Washtenaw County'),
+         ('Michigan', 'Wayne County')] ])
+
+
+def dataFramesManipulation():
+    pass
+
+
+def missingValues():
+    pass
+
 
 if __name__ == '__main__':
-    querying()
+    # series()
+    # querying()
+    # introDataFrames()
+    # dataFramesIndexLoad()
+    # dataFramesQuerying()
+    # dataFramesIndexing()
+    dataFramesManipulation()
+    missingValues()
